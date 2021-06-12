e9d13d5bdeed928b1b6186596d01578d9c6333e2
Shweta2105 <sghadi21@gmail.com>
2021-06-12 09:22:17 +0530

    playing board created

--- com/game/TicTacToeGame.java
+++ com/game/TicTacToeGame.java
@@ -1,17 +1,25 @@
 package com.game;
 
+
+/* @shweta........
+ * building tic tac toe game
+ */
+
 public class TicTacToeGame {
 
 	static char[] board = new char[9];
+	private static void createBoard() {
+		for(int i=0; i<9;i++) {
+			System.out.print(board[i]);
+		}
+	}
 
 	public static void printBoard() {  //printing board
 		System.out.println("---|---|---");
 		System.out.println("|  " + board[0] + " | " + board[1] + " | " + board[2] + "|");
-		System.out.println("|------------|");
-
+		System.out.println("|----------|");
 		System.out.println("|  " + board[3] + " | " + board[4] + " | " + board[5] + "|");
-		System.out.println("|------------|");
-
+		System.out.println("|----------|");
 		System.out.println("|  " + board[6] + " | " + board[7] + " | " + board[8] + "|");
 		System.out.println("---|---|---");
 
@@ -19,7 +27,11 @@ public class TicTacToeGame {
 
 	public static void main(String[] args) {
 		System.out.println("<<<<Welcome to Tic Tac Toe game >>>>");
+		createBoard();
+		System.out.println();
 		printBoard();
 	}
+
+	
 
 }
